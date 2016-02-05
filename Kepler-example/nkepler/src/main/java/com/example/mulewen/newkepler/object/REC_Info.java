d2b49338f34c99ec18a8d9234ea94dfa4fe81e14
mmllmmll8 <mmllmmll8@outlook.com>
2016-02-06 03:00:58 +0800

    fuck google

--- Kepler-example/nkepler/src/main/java/com/example/mulewen/newkepler/object/REC_Info.java
+++ Kepler-example/nkepler/src/main/java/com/example/mulewen/newkepler/object/REC_Info.java
@@ -1,5 +1,7 @@
 package com.example.mulewen.newkepler.object;
 
+import java.util.ArrayList;
+
 public class REC_Info {
 	//��λ�б�
 	//poi���б�
@@ -7,4 +9,6 @@ public class REC_Info {
 	String id;
 	String lbsinfo;
 	String poiinfo;
+	ArrayList<POI_Info> pois;
+	ArrayList<LBSInfo> lbss;
 }
