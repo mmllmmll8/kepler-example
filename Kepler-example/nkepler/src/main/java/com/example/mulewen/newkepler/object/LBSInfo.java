716ba95a2585742f5458d0b2a8a92b944924c438
mmllmmll8 <mmllmmll8@outlook.com>
2016-02-13 20:10:20 +0800

    fuck everything

--- Kepler-example/nkepler/src/main/java/com/example/mulewen/newkepler/object/LBSInfo.java
+++ Kepler-example/nkepler/src/main/java/com/example/mulewen/newkepler/object/LBSInfo.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 
 public class LBSInfo {
 	public String userid;
-	public String lbsid;
 	public double lat;
 	public double lng;
 	public String date;
 	public double accuracy;
-	public boolean haspois = false;
 }
 
